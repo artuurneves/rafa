a6439f3d432ebad5efe19244814306531fca213d
ssbajk <artur.neves@scania.com>
2021-10-11 17:23:21 -0300

    tst

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from app import app
-from flask import render_template, request
+import os
+from flask import Flask,request
+from flask import render_template
 
+
+app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index')
@@ -19,3 +22,13 @@ def calculate():
     low = 100 - value
     recovery = round(((value / low) * 100), 2)
     return render_template('calculadora.html', result=recovery)
+
+
+#teste localhost
+'''if __name__ == '__main__':
+    app.run(debug=True)'''
+
+#teste heroku
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
--- app/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-from app import routes
-
-
-app.run(debug=True, port=8080, host='192.168.15.11')
